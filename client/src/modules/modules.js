2d02b1ce8d25344575e65f280277e838b3e6b079
EmanFun <emanuelfun.95@gmail.com>
2023-10-07 09:05:37 -0300

    create client vite project

---
+++ client/src/modules/modules.js
@@ -0,0 +1,8 @@
+/*
+Organismos
+Los organismos son conjuntos de moléculas y átomos que crean elementos más complejos de una interfaz.
+
+Por ejemplo, una cabecera contiene moléculas y átomos, como un logotipo, la navegación principal y de su sección, la selección del idioma o un formulario de búsqueda.
+
+Organismos son elementos reutilizados asiduamente como cabeceras y pies, un menú completo de navegación o un listado de elementos.
+ */
