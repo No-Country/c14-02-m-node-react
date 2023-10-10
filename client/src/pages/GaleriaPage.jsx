201f122ea9280f93cbe158b3a45906dff3d59da1
Tiziano Mina <tizianominajo@gmail.com>
2023-10-10 13:19:02 -0300

    Se realizo la página en la cual va a contener las cards

---
+++ client/src/pages/GaleriaPage.jsx
@@ -0,0 +1,21 @@
+import { Card } from "../components/Card";
+
+export const GaleriaPage = () => {
+  return (
+    <section className="conteinerCards flex flex-wrap w-ful p-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        
+    </section>
+  );
+};
