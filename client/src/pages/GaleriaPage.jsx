65436baa375575714d1e900435b52cf7292d33e1
Tiziano Mina <tizianominajo@gmail.com>
2023-10-10 16:25:04 -0300

    Se iteran las card con el JSON

--- client/src/pages/GaleriaPage.jsx
+++ client/src/pages/GaleriaPage.jsx
@@ -1,21 +1,12 @@
 import { Card } from "../components/Card";
+import { dataCards } from "../data/data";
 
 export const GaleriaPage = () => {
   return (
     <section className="conteinerCards flex flex-wrap w-ful p-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        
+      {dataCards.map((cardElement) => (
+        <Card key={cardElement.id} cardElement={cardElement} />
+      ))}
     </section>
   );
 };
