821c4b36611ea70728c60a9c6ceadf911307d394
Tiziano Mina <tizianominajo@gmail.com>
2023-10-10 13:18:07 -0300

    Se hizo un componente para mostrar una breve info.

---
+++ client/src/components/CardInfo.jsx
@@ -0,0 +1,18 @@
+export const CardInfo = () => {
+  return (
+    <section className="mt-2">
+      <div className="flex justify-between text-[#222]">
+        <p className="font-bold">Provincia de Buenos Aires, Argentina</p>
+        <p>&#9733; 4,92</p>
+      </div>
+
+      <div>
+        <p className="text-[#858484]">A 398 kilómetros de distancia</p>
+        <p className="text-[#858484]">10 del 15 oct.</p>
+        <p className="mt-3 text-[#222]">
+          <b>$246 USD</b> noche
+        </p>
+      </div>
+    </section>
+  );
+};
