5e532a408dc26285373df1429c6e84eac2f10f50
EmanFun <emanuelfun.95@gmail.com>
2023-10-10 21:41:01 -0300

    now yes, the commit of model and dao

--- Servidor/src/models/index.js
+++ Servidor/src/models/index.js
@@ -1,4 +1,5 @@
 const UserModel = require('./user.model');
 // const pictureModel = require('./picture-model');
+const ReviewModel = require('./review.model');
 
-module.exports = { UserModel };
+module.exports = { UserModel, ReviewModel };
