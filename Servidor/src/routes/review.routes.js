8d1ca180ce8c6bfdd60bb1842b484d45fc43030a
EmanFun <emanuelfun.95@gmail.com>
2023-10-11 09:34:42 -0300

    added createReview method and fixes minor

--- Servidor/src/routes/review.routes.js
+++ Servidor/src/routes/review.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const [ ReviewController ]= require('../controllers');
+const {ReviewController} = require('../controllers');
 
 
 router.get('/', ReviewController.getAllReviews);
