fbee1a0386bd9fa9debe313a1a071bdc15742b44
Tiziano Mina <tizianominajo@gmail.com>
2023-10-15 20:02:51 -0300

    se muestran las paginas de login-registro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-import Filtros from "./components/Filtros";
-import NavBar from "./components/NavBar";
-import { GaleriaPage } from "./pages/GaleriaPage";
+import {CrearPerfil} from "./pages/Pages-login-register/CrearPerfil";
+import { FinishSigningUp } from "./pages/Pages-login-register/FinishSigningUp";
+import { AccessPage1 } from "./pages/Pages-login-register/AccessPage1";
+import { AccessPage2 } from "./pages/Pages-login-register/AccessPage2";
+import {CreateYourProfile} from "./pages/Pages-login-register/CreateYourProfile";
+import { CommunityLoginPage } from "./pages/Pages-login-register/CommunityLoginPage";
+import { CaptchaNumber } from "./pages/Pages-login-register/CaptchaNumber";
+import {AddPhoto} from "./pages/Pages-login-register/AddPhoto";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Filtros />
-      <GaleriaPage />
+      <AccessPage1 />
+      <AccessPage2 />
+      <FinishSigningUp />
+      <CommunityLoginPage />
+      <CrearPerfil />
+      <CreateYourProfile />
+      <CaptchaNumber />
+      <AddPhoto />
+      
     </>
   );
 }
