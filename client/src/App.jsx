af539fd41498a51e1bc248a0a39be1f6b2d31baf
Tiziano Mina <tizianominajo@gmail.com>
2023-10-19 04:38:29 -0300

    Se realizo un nuevo ruteo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import "./index.css";
+import { AccessPage1 } from "./pages/AccessPage1";
+import { AuthProvider } from "./context/AuthContext";
+import UserProfile from "./pages/UserProfile";
+import IndividualViewPage from "./pages/IndividualViewPage";
 
 function App() {
   return (
+    <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<AccessPage1 />} />
+        <Route path="/user" element={<UserProfile />} />
+        <Route path="/rooms" element={<IndividualViewPage />} />
       </Routes>
+    </AuthProvider>
   );
 }
 
