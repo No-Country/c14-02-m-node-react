7073f2ccde8d5c908f48a503592d17abb7e1ff65
VaneSanchez <nyagami@yahoo.com>
2023-10-19 13:36:21 -0300

    commit App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,12 @@
-import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import "./index.css";
-import { AccessPage1 } from "./pages/AccessPage1";
-import { AuthProvider } from "./context/AuthContext";
-import UserProfile from "./pages/UserProfile";
-import IndividualViewPage from "./pages/IndividualViewPage";
+
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<AccessPage1 />} />
-        <Route path="/user" element={<UserProfile />} />
-        <Route path="/rooms" element={<IndividualViewPage />} />
       </Routes>
-    </AuthProvider>
   );
 }
 
