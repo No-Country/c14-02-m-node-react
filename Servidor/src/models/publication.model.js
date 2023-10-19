2913d1fd225a1c3a60fb0683743120bbbcc235f1
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-10-19 20:08:31 -0300

    Validaciónes terminadas para Publicaciónes

--- Servidor/src/models/publication.model.js
+++ Servidor/src/models/publication.model.js
@@ -59,33 +59,92 @@ const publicationScheme = new mongoose.Schema({
 		required: true,
 		enum:['Guardia de seguridad','Cámara de vigilancia','Sistema de alarmas']
 	},
-	photos: {
-		type: [String],
-		required: true,
-	},
 	title: {
 		type: String,
 		required: true,
+		validate: [
+			{
+			  validator: function (value) {
+				return value.length >= 5;
+			  },
+			  message: 'El título debe tener al menos 5 caracteres.',
+			},
+			{
+			  validator: function (value) {
+				return value.length <= 100;
+			  },
+			  message: 'El título no debe exceder los 100 caracteres.',
+			},
+		  ],
 	},
 	description: {
 		type: String,
 		required: true,
+		validate: [
+			{
+			  validator: function (value) {
+				return value.length >= 10;
+			  },
+			  message: 'La descripción debe tener al menos 10 caracteres.',
+			},
+			{
+			  validator: function (value) {
+				return value.length <= 1000; // Ajusta la longitud máxima según tus necesidades.
+			  },
+			  message: 'La descripción no debe exceder los 1000 caracteres.',
+			},
+		],
 	},
 	type_guest: {
 		type: String,
 		required: true,
+		validate: [
+			{
+			  validator: function (value) {
+				return value.length >= 5;
+			  },
+			  message: 'La descripción debe tener al menos 5 caracteres.',
+			},
+			{
+			  validator: function (value) {
+				return value.length <= 50; // Ajusta la longitud máxima según tus necesidades.
+			  },
+			  message: 'La descripción no debe exceder los 50 caracteres.',
+			},
+		],
+
 	},
 	price: {
 		type: String,
 		required: true,
+		validate: [
+			{
+			  validator: function (value) {
+				// Utiliza una expresión regular para verificar que el precio contenga solo números y un punto decimal (si corresponde)
+				return /^\d+(\.\d+)?$/.test(value);
+			  },
+			  message: 'El precio solo puede contener números y un punto decimal con uno o más decimales.',
+			},
+		  ],
 	},
 	discount: {
 		type: String,
 		required: true,
+		validate: [
+			{
+			  validator: function (value) {
+				// Convierte el valor a un número y verifica que esté en el rango de 5 a 100
+				const numericValue = parseFloat(value);
+				return !isNaN(numericValue) && numericValue >= 5 && numericValue <= 100;
+			  },
+			  message: 'El descuento debe ser entre 5% y 100%',
+			},
+		  ],
 	},
 	extra_Security: {
 		type: [String], // Cambiado a Array de Strings
 		required: true,
+		enum:['Se requiere depósito de seguridad']
 	},
 	email: {
 		type: String,
