0edd9e9cecd425bdd7b346bc4ff1401b8cbdad3a
Ivana-Antonella <antonellacarmona090@gmail.com>
2023-10-21 14:23:32 -0300

    Trabajando en las validaciones de User

--- Servidor/src/models/user.model.js
+++ Servidor/src/models/user.model.js
@@ -7,42 +7,69 @@ const userScheme = new mongoose.Schema({
 	names: {
 		type: String,
 		required: true, // Este campo es obligatorio
+		trim: true, // Elimina espacios en blanco al principio y al final
 		
 	},
 
 	surname: {
 		type: String,
 		required: true,
+		trim: true, 
 	},
 
 	birthDate: {
 		type: String,
 		required: true,
+		validate: {
+			validator: (value) => /\d{2}\/\d{2}\/\d{4}/.test(value), // Valida el formato de fecha (DD/MM/YYYY)
+			message: 'Formato de fecha no válido (DD/MM/YYYY)',
+		},
 	},
 
 	email: {
 		type: String,
 		required: true,
+		trim: true,
+		validate: {
+			validator: isEmail, // Utiliza la función isEmail del paquete 'validator'
+			message: 'El correo electrónico no es válido',
+		},
 	},
 
 	phone: {
 		type: String,
 		required: true,
+		trim: true,
+		validate: {
+			validator: (value) => isMobilePhone(value, 'any', { strictMode: false }), // Valida el formato del número de teléfono
+			message: 'Número de teléfono no válido',
+		},
 	},
 
 	address: {
 		type: String,
 		required: true,
+		trim: true,
 	},
 
 	officialId: {
 		type: String,
 		required: true,
+		trim: true,
+		validate: {
+			validator: (value) => /\d{8}-\d{1}/.test(value), // Valida el formato de identificación (XXXXXXXX-X)
+			message: 'Formato de identificación no válido (XXXXXXXX-X)',
+		},
 	},
 
 	phoneUrgency: {
 		type: String,
 		required: true,
+		trim: true,
+		validate: {
+			validator: (value) => isMobilePhone(value, 'any', { strictMode: false }), // Valida el formato del número de teléfono
+			message: 'Número de teléfono de emergencia no válido',
+		},
 	},
 
 	// Referencia a la imagen de perfil del usuario almacenada en la colección 'picture'
