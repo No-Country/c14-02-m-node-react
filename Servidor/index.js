772fa11a282701432ff81fb0cf3f67dce3aceece
EmanFun <emanuelfun.95@gmail.com>
2023-10-21 12:30:16 -0300

    added cors middleware

--- Servidor/index.js
+++ Servidor/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
@@ -15,13 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(cors());
 
 // Routes
 app.use('/api/user', userRoutes);
