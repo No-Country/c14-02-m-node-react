a78fd396647d858ceeb2c980c919240e0539d7a4
VaneSanchez <nyagami@yahoo.com>
2023-10-24 11:40:12 -0300

    API key hidden

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -11,6 +11,6 @@ const firebaseConfig = {
   measurementId: "G-P4MVNSS8H3"
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
