462a0bc262a1ff09465ea5f2169ae0b2621560b2
emybr <emybr82ar@gmail.com>
2023-10-24 23:00:13 -0300

    ivp grid ok

--- client/src/pages/IndividualViewPage.jsx
+++ client/src/pages/IndividualViewPage.jsx
@@ -78,7 +78,7 @@ function IndividualViewPage(props) {
           </div>
         </div>
 
-        <IvpGrid images={data.photos}/>
+        <IvpGrid images={data?.photos || []}/>
         <IvpDescription />
         <IvpMaps />
         <IvpReviews />
