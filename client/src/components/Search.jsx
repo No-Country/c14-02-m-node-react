21eb6c4e4a6bb8edf233dc4b1cc1c9deef377fe2
Tiziano Mina <tizianominajo@gmail.com>
2023-10-27 13:38:34 -0300

    Se creo una función para cambiar de pagina

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useSearch } from "../hook/useSearch";
 
 export const Search = () => {
-  const { searchQuery, handleSearchChange, filteredLocations } = useSearch();
+  const { searchQuery, handleSearchChange } = useSearch();
+  const navigate = useNavigate(); // Obtiene una función para cambiar de página en la aplicación.
 
-  console.log(filteredLocations);
-
+  const handleSearch = () => {
+    if (searchQuery) {
+      navigate(`/searchfilter?query=${searchQuery}`);
+    }
+  };
+  // La función handleSearch se ejecuta cuando el botón de búsqueda se hace clic. Verifica si searchQuery tiene un valor válido y, si es así, utiliza navigate para redirigir a la página de resultados de búsqueda con la consulta de búsqueda en la URL.
+  
   return (
-    <form  className="w-96 flex">
+    <form className="w-96 flex">
       <input
         type="text"
         value={searchQuery}
@@ -16,10 +23,11 @@ export const Search = () => {
         className="p-2 border-none rounded-l-full w-full"
       />
       <button
-        type="submit"
+        type="button"
+        onClick={handleSearch}
         className="p-2 bg-red-500 text-white rounded-r-full"
       >
-        <Link to="/searchfilter">Buscar</Link>
+        Buscar
       </button>
     </form>
   );
