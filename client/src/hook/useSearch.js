1d484949d2515df68b7f2325450e6fb656acd829
Tiziano Mina <tizianominajo@gmail.com>
2023-10-27 11:52:57 -0300

    Se creo un hook personalizado

---
+++ client/src/hook/useSearch.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useGetAllPublicationsQuery } from "../store/rtk-query";
+
+
+export function useSearch() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const { data: publications } = useGetAllPublicationsQuery();
+  const locations = publications.map(publication => publication.location);
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value.toLowerCase());
+  }
+
+  const filteredLocations = locations.filter(location => location.toLowerCase().includes(searchQuery));
+
+
+
+  return {
+    searchQuery,
+    handleSearchChange,
+    filteredLocations
+  }
+}
