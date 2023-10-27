44aa917fed38bf33d0f140ee90c1f090ad96fd1a
Tiziano Mina <tizianominajo@gmail.com>
2023-10-27 11:52:39 -0300

    Se creo el componente de busqueda y se agrego el useSearch (Hook personalizado)

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,20 +1,25 @@
-// import { useGetAllPublicationsQuery } from "../store/rtk-query";
-// location
+import { Link } from "react-router-dom";
+import { useSearch } from "../hook/useSearch";
+
 export const Search = () => {
-  
+  const { searchQuery, handleSearchChange, filteredLocations } = useSearch();
+
+  console.log(filteredLocations);
 
   return (
-    <form className="w-96 flex">
+    <form  className="w-96 flex">
       <input
         type="text"
+        value={searchQuery}
+        onChange={handleSearchChange}
         placeholder="Buscar alojamiento.."
         className="p-2 border-none rounded-l-full w-full"
       />
       <button
-        type="button"
+        type="submit"
         className="p-2 bg-red-500 text-white rounded-r-full"
       >
-        Buscar
+        <Link to="/searchfilter">Buscar</Link>
       </button>
     </form>
   );
