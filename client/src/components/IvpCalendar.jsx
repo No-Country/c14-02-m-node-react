2840c6cb5f4ba0bf357db774f1930c4ec1e284ab
emybr <emybr82ar@gmail.com>
2023-10-28 09:13:02 -0300

    cambio en calendar

--- client/src/components/IvpCalendar.jsx
+++ client/src/components/IvpCalendar.jsx
@@ -3,6 +3,9 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import Time from "./Time";
 
+// Importa la configuración de idioma para español
+import { es } from 'date-fns/locale';
+
 function IvpCalendar() {
   const [date, setDate] = useState(new Date());
   const [showTime, setShowTime] = useState(false);
@@ -11,29 +14,31 @@ function IvpCalendar() {
     <div className="app mx-3">
       <h1 className="header text-2xl font-semibold mt-5">Haga su reserva:</h1>
       <div className="text-stone-600 font-semibold my-5">
-      {date.length > 0 ? (
-   <p>
-     <span>Start:</span>
-     {date[0].toDateString()}
-     &nbsp;
-     &nbsp;
-     <span>End:</span>{date[1].toDateString()}
-   </p>
-          ) : (
-   <p>
-    {date.toDateString()}
-   </p> 
-          )
-   }
+        {date.length > 0 ? (
+          <p>
+            <span>Inicio:</span>
+            {date[0].toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+            &nbsp;
+            &nbsp;
+            <span>Fin:</span>
+            {date[1].toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+          </p>
+        ) : (
+          <p>
+            {date.toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
+          </p>
+        )}
       </div>
       <div className="calendar-container font-semibold">
         <Calendar
           onChange={setDate}
           value={date}
           onClickDay={() => setShowTime(true)}
+          // Configura el idioma en español
+          locale={es}
         />
       </div>
-      <Time showTime={showTime} date={date}/>
+      <Time showTime={showTime} date={date} />
     </div>
   );
 }
