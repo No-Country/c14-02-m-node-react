e32d99a4e389cb08f4fb21b2684d08f08227b099
VaneSanchez <nyagami@yahoo.com>
2023-10-29 09:06:27 -0300

    Agregando componentes a la Vista de propiedad

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -14,22 +14,17 @@ function UserProfile() {
   const [userData, setUserData] = useState(null);
   const [showRegister, setShowRegister] = useState(false);
 
-
-
   useEffect(() => {
-    
-    // if (!isLoading) {
-      console.log(data)
+    if (!isLoading) {
       if (data && data.email === user.email) {
         // El usuario está registrado, no es necesario mostrar el componente de registro.
         setUserData(data);
-        console.log(data)
         setShowRegister(false);
       } else {
         // El usuario no está registrado, muestra el componente de registro.
         setShowRegister(true);
       }
-    // }
+    }
   }, [data, isLoading, user.email]);
 
   if (isLoading) {
@@ -43,18 +38,20 @@ function UserProfile() {
       ) : (
         <>
           <NavBar photoPerson={user.photoURL} />
-          <UProfile 
-            names={data.names}
-            surname={data.surname}
-            birthDate={data.birthDate}
-            email={data.email}
-            phone={data.phone}
-            address={data.address}
-            phoneUrgency={data.phoneUrgency}
-            photo={data.photo} 
-            // photo={user.photoURL} 
-             />
-            
+          {data ? (
+            <UProfile 
+              names={data.names}
+              surname={data.surname}
+              birthDate={data.birthDate}
+              email={data.email}
+              phone={data.phone}
+              address={data.address}
+              phoneUrgency={data.phoneUrgency}
+              photo={data.photo} 
+            />
+          ) : (
+            <div>Loading...</div>
+          )}
           <HelpFooter />
           <SuggestionFooter />
         </>
