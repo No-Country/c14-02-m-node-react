16e7bee7e083119363f4452ec6c40bf4daf08fce
Tiziano Mina <tizianominajo@gmail.com>
2023-11-02 16:25:01 -0300

    Se modifico el (Pone tu airbnb) solo para los que estan logueado

--- client/src/components/BottonMenu.jsx
+++ client/src/components/BottonMenu.jsx
@@ -1,32 +1,29 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+// import styled from "styled-components";
 import menu from "../assets/images/menu.png";
 import usuario from "../assets/images/usuario.png";
 import { useAuth } from "../context/AuthContext";
 
+// const DivMenu = styled.div`
+// 	display: flex;
+// 	align-items: start;
+// 	position: absolute;
+// 	justify-content: space between;
+// 	padding: 10px 0;
+// 	margin-top: 10px;
+// 	transform: translateX(-66%);
+// 	width: auto;
+// 	height: auto;
+// 	flex-shrink: 0;
+// 	border-radius: 14px;
+// 	box-shadow: 0px 4px 18px 0px rgba(0, 0, 0, 0.17);
+// 	background-color: white;
+// 	z-index: 100;
 
-
-const DivMenu = styled.div`
-	display: flex;
-	align-items: start;
-	position: absolute;
-	justify-content: space between;
-	padding: 10px 0;
-	margin-top: 10px;
-	transform: translateX(-66%);
-	width: auto;
-	height: auto;
-	flex-shrink: 0;
-	border-radius: 14px;
-	box-shadow: 0px 4px 18px 0px rgba(0, 0, 0, 0.17);
-	background-color: white;
-	z-index: 100;
-
-	&:hover {
-	}
-`;
-
+// 	&:hover {
+// 	}
+// `;
 
 const Component = ({ photoPerson }) => {
 	const { user, logout } = useAuth();
@@ -67,31 +64,46 @@ const Component = ({ photoPerson }) => {
 	return (
 		<div>
 			<button onClick={toggleMenu}>
-				<div className="flex p-2  items-center gap-3 rounded-3xl border border-gray-300 bg-white cursor-pointer hover:shadow-lg hover:border-gray-400 md:z-50" selected={selected} onClick={() => setSelected(!selected)}>
+				<div
+					className="flex p-2  items-center gap-3 rounded-3xl border border-gray-300 bg-white cursor-pointer hover:shadow-lg hover:border-gray-400 md:z-50"
+					selected={selected}
+					onClick={() => setSelected(!selected)}
+				>
 					<img src={menu} />
-					<img className="md:w-8 w-10 rounded-full" src={user ? photoPerson : usuario} /> {/* Aplica el estilo a la imagen aquí */}
+					<img className="md:w-8 w-10 rounded-full" src={user ? photoPerson : usuario} />{" "}
+					{/* Aplica el estilo a la imagen aquí */}
 				</div>
 			</button>
 			{isMenuOpen && (
-				<div className="flex absolute translate-x-[-69%] w-auto h-auto shrink-0 shadow-[0px_4px_18px_0px_rgba(0,0,0,0.17)] bg-[white] z-[100] mt-2.5 px-0 py-2.5 rounded-[14px]" >
+				<div className="flex absolute translate-x-[-69%] w-auto h-auto shrink-0 shadow-[0px_4px_18px_0px_rgba(0,0,0,0.17)] bg-[white] z-[100] mt-2.5 px-0 py-2.5 rounded-[14px]">
 					{/* 0 90px 0 10px */}
 					<div className="p-2 flex flex-col m-0 ml-3 mr-20   gap-4">
 						{user ? ( // Verifica si el usuario está autenticado
 							<>
 								<Link to="/user">
-									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">Mi Perfil</p>
+									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">
+										Mi Perfil
+									</p>
 								</Link>
 								<Link to="/favorites" className="mb-2">
-									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">Favoritos</p>
+									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">
+										Favoritos
+									</p>
 								</Link>
 								<Link to="/manageads" className="mb-2">
-									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">Administrar anuncios</p>
+									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">
+										Administrar anuncios
+									</p>
 								</Link>
 								<Link to="/tuairbnb" className="mb-2">
-									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">Poné tu Airbnb</p>
+									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">
+										Poné tu Airbnb
+									</p>
 								</Link>
 								<Link to="/centrodeayuda" className="mb-2">
-									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">Centro de Ayuda</p>
+									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70">
+										Centro de Ayuda
+									</p>
 								</Link>
 								<div>
 									<button className="text-red-600" onClick={handlelogout}>
@@ -102,17 +114,28 @@ const Component = ({ photoPerson }) => {
 						) : (
 							<>
 								<Link to="/register">
-									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70" classNameName="mb-2 font-medium" onClick={() => openModal(1)}>
+									<p
+										className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70"
+										onClick={() => openModal(1)}
+									>
 										Registrate
 									</p>
 								</Link>
 								<Link to="/register">
-									<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70" classNameName="border-b mb-2 pb-5" onClick={() => openModal(2)}>
+									<p
+										className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70 border-b mb-2 pb-5"
+										onClick={() => openModal(2)}
+									>
 										Inicia Sesión
 									</p>
 								</Link>
-								<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70" onClick={() => openModal(3)}>Poné tu Airbnb</p>
-								<p className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70" onClick={() => openModal(4)}>Centro de Ayuda</p>
+								
+								<p
+									className="flex whitespace-nowrap m-0 text-base cursor-pointer hover:opacity-70"
+									onClick={() => openModal(4)}
+								>
+									Centro de Ayuda
+								</p>
 							</>
 						)}
 					</div>
